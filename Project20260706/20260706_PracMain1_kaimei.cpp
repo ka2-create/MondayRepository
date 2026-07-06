e35fa10de04eb5d398236ed74c64369374611366
ka2-create <kaimei042655@icloud.com>
2026-07-06 12:23:55 +0900

    演習１

---
+++ Project20260706/20260706_PracMain1_kaimei.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+#include"20260706_Header1_kaimei.h"
+using namespace std;
+
+int main()
+{
+	//ÀsŠÖ”
+	Run();
+	return 0;
+}
---
+++ Project20260706/Config.h
@@ -0,0 +1,17 @@
+#pragma once
+//名前空間で定数を保持
+namespace ConstNumber
+{
+	//playreHP基本値
+	const int PLAYER_HP = 100;
+	//HP回復値
+	const int HEAL_POINT = 20;
+	//選択最小値
+	const int SELECT_MIN = 1;
+	//選択最大値
+	const int SELECT_MAX = 2;
+}
+//?名前空間
+//一番早くコンパイルしますほかに影響する
+//コンパイルとは、人間が書いたプログラム（ソースコード
+// をコンピュータが実行できる機械語（オブジェクトコード）に変換する作業です。
